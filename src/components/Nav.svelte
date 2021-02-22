303049be138f859d17a136e786ef27a780ad2c9a
royghu201901 <royghu201901@hotmail.com>
2021-02-22 11:40:43 +0800

    set pages, components and nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,3 +1,59 @@
 <script>
 	export let segment;
+	let isOpen = false;
+	function toggleNav() {
+		isOpen = !isOpen
+	}
 </script>
+
+<style>
+	@media(min-width: 769px) {
+		nav {
+			border-bottom: 1px solid gray;
+		}
+	}
+</style>
+
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="/">
+      <img src="logo.png" alt="logo" width="30" height="30">
+    </a>
+
+    <span
+			role="button"
+			class="navbar-burger"
+			class:is-active={isOpen}
+			aria-label="menu"
+			aria-expanded="false"
+			on:click={toggleNav}
+		>
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+		</span>
+  </div>
+
+  <div
+		class="navbar-menu"
+		class:is-active={isOpen}
+	>
+    <div class="navbar-start">
+      <a 
+				class="navbar-item"
+				class:is-active={segment === undefined}
+				href="/"
+			>
+        Home
+      </a>
+
+      <a
+				class="navbar-item"
+				class:is-active={segment === 'about'}
+				href="/about"
+			>
+        About
+      </a>
+    </div>
+  </div>
+</nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,15 @@
+<script>
+	import CovidStat from '../components/CovidStat.svelte'
+	import CovidChart from '../components/CovidChart.svelte'
+	import TableContainer from '../components/TableContainer.svelte'
+</script>
+
 <svelte:head>
 	<title>Roy Gang Hu's Covid Report Demo by Sevlte</title>
 </svelte:head>
+
+<CovidStat />
+
+<CovidChart />
+
+<TableContainer />
